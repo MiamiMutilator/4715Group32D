ce7706c2222225a4a95f3b532022b56668198199
ralfomanzur <101080306+MiamiMutilator@users.noreply.github.com>
2025-02-03 12:23:37 -0500

    Simple Stuff
    Added an "about" scene, scene transitions script, working menu buttons

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    //Ralfo Manzur
+    //very simple script comprised of functions that can be used by the editor/application to load scenes, can be called/used in a variety of ways
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadAbout()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
