4b45c2dc8acfc45e8b1bb9059851c1d06fafa422
alopez0213122 <alopez0213122@gmail.com>
2025-02-12 01:54:05 -0500

    Pickups Updated
    -removed blink from player
    -blink functions are now on player interaction script
    - added invincibility and its pickup prefab
    - added blink pickup prefab
    - added ability to lose powerup if hit
    - added goal prefab

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -5,25 +5,61 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     public int health = 10;
+    public bool isInvincible = false;
+    public int invincibilityTime = 5;
+
+    //blink stuff
+    public float distance = 10f;
+    public GameObject trail;
+    public bool canBlink = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Physics2D.queriesStartInColliders = false; // blink
     }
 
     // Update is called once per frame
     void Update()
     {
+        //blink stuff
+        if (Input.GetKeyDown(KeyCode.E) && canBlink == true)
+        {
+            Debug.Log("Key Pressed");
 
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.localScale.x * Vector2.right, distance);
+
+            if (hit.collider == null)
+            {
+                transform.position += transform.localScale.x * Vector3.right * distance;
+
+            }
+            else
+            {
+                transform.position = hit.point;
+            }
+            StartCoroutine(LoseTime());
+
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy" && isInvincible == true)
         {
-            Debug.Log("Triggered");
-            health--;
+            Destroy(collision.gameObject);
+            Debug.Log("Defeated");
+
         }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Took Damage!");
+            health--;
+            canBlink = false;
+        }
+
+
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -37,6 +73,48 @@ public class PlayerInteraction : MonoBehaviour
         {
             Destroy(collision.gameObject);
             Debug.Log("Finished");
+        }
+        if (collision.gameObject.tag == "Invincible")
+        {
+            isInvincible = true;
+            Destroy(collision.gameObject);
+            Debug.Log("Is Invincible!");
+            StartCoroutine(LoseInvincibilityTime());
+        }
+        if (collision.gameObject.tag == "Blink")
+        {
+            canBlink = true;
+            Destroy(collision.gameObject);
+            Debug.Log("Can Blink!");
+        }
+    }
+
+    //blink stuff
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawLine(transform.position, transform.position + transform.localScale.x * Vector3.right * distance);
+    }
+    IEnumerator LoseInvincibilityTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(invincibilityTime);
+            isInvincible = false;
+            yield break;
+        }
+    }
+
+    //blink stuff
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            trail.SetActive(true);
+            yield return new WaitForSeconds(0.2f);
+            trail.SetActive(false);
+            yield break;
         }
     }
 }
